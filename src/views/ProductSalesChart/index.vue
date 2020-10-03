8a4009c3e344465ec6f3b687b6705e17c0eeda97
子鵬 李 <hawk419383@gmail.com>
2020-10-03 22:22:18 +0800

    change port

--- src/views/ProductSalesChart/index.vue
+++ src/views/ProductSalesChart/index.vue
@@ -5,7 +5,7 @@
   import Chart from 'chart.js'
   // import '../../assets/chartjs/productchart.js'
   // import '../../assets/chartjs/productclasschart.js'
-  const url = 'https://localhost:5001/'
+  const url = 'http://sgwebapi-core.azurewebsites.net'
   const SalesQuantity = url + 'api/Chart/SalesQuantity'
   const MonthlyTransactions = url + 'api/Chart/MonthlyTransactions'
   const ClassSalesQuantity = url + 'api/Chart/ClassSalesQuantity'
